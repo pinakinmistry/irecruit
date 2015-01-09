1d3b06cada701c36e36e29640847aa3e8aff7301
pinakinmistry <pinakin.nmistry@gmail.com>
2015-01-09 17:34:11 +0530

    Initial scafoldded app

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name irecruitApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the irecruitApp
+ */
+angular.module('irecruitApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name irecruitApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the irecruitApp
+ */
+angular.module('irecruitApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
