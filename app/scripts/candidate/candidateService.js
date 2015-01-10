a1c399da6b8572f34ba1f9bf0f593e92f7e7eff2
pinakinmistry <pinakin.nmistry@gmail.com>
2015-01-10 13:02:32 +0530

    Test questions hard coded for testing

--- app/scripts/candidate/candidateService.js
+++ app/scripts/candidate/candidateService.js
@@ -5,8 +5,7 @@ angular.module('news.candidate.services', [
 	.factory('Candidate', function () {
 	  
 	  var candidates = [
-	  	{ name: 'Pinakin Mistry', phone: '9819515125', email: 'pmistry@gmail.com', skypeId: 'pmistry', skills: 'Front end' },
-	  	{ name: 'Aditya Sethi', phone: '9819515126', email: 'asethi@gmail.com', skypeId: 'asethi', skills: 'Front end' }
+	  	{ name: 'Pinakin Mistry', phone: '9819515125', email: 'pmistry@gmail.com', skypeId: 'pmistry', skills: 'Front end' }
 	  ];
 	  var Candidate = {
 	    all: candidates,
--- app/scripts/test/testService.js
+++ app/scripts/test/testService.js
@@ -5,7 +5,36 @@ angular.module('news.test.services', [
 	.factory('Test', function () {
 	  
 	  var tests = [
-	  	{ skills: 'Front end', name: 'HTML, CSS, JavaScript v1', creator: 'Ashish Juyal', type: 'Initial screening' },
+	  	{ skills: 'Front end', name: 'HTML, CSS, JavaScript v1', creator: 'Ashish Juyal', type: 'Initial screening',
+	  	  questions: [
+	  	  	{
+		  	  	type: 'Single Answer',
+		  	  	text: 'Q1',
+		  	  	options: [
+		  	  		{text: 'o1'},
+		  	  		{text: 'o2'},
+		  	  		{text: 'o3'}
+		  	  	]
+		  	  },
+		  	  {
+		  	  	type: 'Multiple Answers',
+		  	  	text: 'Q2',
+		  	  	options: [
+		  	  		{text: 'o1'},
+		  	  		{text: 'o2'},
+		  	  		{text: 'o3'}
+		  	  	]
+		  	  },
+		  	  {
+		  	  	type: 'Textual',
+		  	  	text: 'T1'
+		  	  },
+		  	  {
+		  	  	type: 'Exercise',
+		  	  	text: 'Exercise 1'
+		  	  }
+	  	  ]
+	  	},
 	  	{ skills: 'Front end', name: 'HTML, CSS, JavaScript v2', creator: 'Abhinav Kumar', type: 'Tech round' }
 	  ];
 	  var Test = {
